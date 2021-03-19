79b3479a0372e4fae54cce10d8f68bfdd43394e9
sytyd <n.yakovlev@g.nsu.ru>
2021-03-19 15:29:34 +0700

    First Version Local Server

---
+++ BeerMe/taskmanager/main/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+
+from .forms import TaskForm
+from .models import Task
+
+
+# Create your views here.
+
+
+
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'main/index.html', {'title' : ' Главная страница сайта', 'tasks':tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def review(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = "Incorrect"
+
+    form = TaskForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'main/review.html',context)
